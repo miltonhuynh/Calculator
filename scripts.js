8d2a18c06bbc40f64f771425932dd796e23d4c40
miltonhuynh <miltonhuynh@gmail.com>
2022-05-15 10:12:23 -0700

    Started onclick

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const num1 = 0;
+const num2 = 0;
+
+const digit_0 = document.getElementById('digit_0');
+const digit_1 = document.getElementById('digit_1');
+const digit_2 = document.getElementById('digit_2');
+const digit_3 = document.getElementById('digit_3');
+const digit_4 = document.getElementById('digit_4');
+const digit_5 = document.getElementById('digit_5');
+const digit_6 = document.getElementById('digit_6');
+const digit_7 = document.getElementById('digit_7');
+const digit_8 = document.getElementById('digit_8');
+const digit_9 = document.getElementById('digit_9');
+
+document.body.style.backgroundColor = "rgb(0,0,75)";
+digit_0.onclick;
