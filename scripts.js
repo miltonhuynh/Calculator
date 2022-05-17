f501e608b587b1976c094ac383289162f1534ff6
miltonhuynh <miltonhuynh@gmail.com>
2022-05-16 21:48:01 -0700

    Displayed all numbers

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 let num1 = null;
 let num2 = null;
 
+const clear = document.getElementById('clear');
 const equal = document.getElementById('equal');
+
+const add = document.getElementById('addition');
+const sub = document.getElementById('subtract');
+const mult = document.getElementById('multiply');
+const divide = document.getElementById('divide');
 
 const digit_0 = document.getElementById('digit_0');
 const digit_1 = document.getElementById('digit_1');
@@ -14,13 +20,35 @@ const digit_7 = document.getElementById('digit_7');
 const digit_8 = document.getElementById('digit_8');
 const digit_9 = document.getElementById('digit_9');
 
+add.onclick = function() {
+    num1 = parseInt(num1);
+}
+
+clear.onclick = function() {
+    num1 = 0;
+    document.getElementById('display').innerHTML = num1;
+}
+
+digit_0.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 0;
+        num1 = num1.toString();
+    } else {
+        num1 += 0;
+        num1 = num1.toString();
+    }
+    document.getElementById('display').innerHTML = num1;
+}
+
 digit_1.onclick = function() {
     if (num1 === null && num2 === null) {
         num1 = 1;
         num1 = num1.toString();
     } else {
         num1 += 1;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_2.onclick = function() {
@@ -29,7 +57,9 @@ digit_2.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 2;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_3.onclick = function() {
@@ -38,7 +68,9 @@ digit_3.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 3;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_4.onclick = function() {
@@ -47,7 +79,9 @@ digit_4.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 4;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_5.onclick = function() {
@@ -56,7 +90,9 @@ digit_5.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 5;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_6.onclick = function() {
@@ -65,7 +101,9 @@ digit_6.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 6;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_7.onclick = function() {
@@ -74,7 +112,9 @@ digit_7.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 7;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_8.onclick = function() {
@@ -83,7 +123,9 @@ digit_8.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 8;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
 }
 
 digit_9.onclick = function() {
@@ -92,5 +134,10 @@ digit_9.onclick = function() {
         num1 = num1.toString();
     } else {
         num1 += 9;
+        num1 = num1.toString();
     }
+    document.getElementById('display').innerHTML = num1;
+}
+
+equal.onclick = function() {
 }
