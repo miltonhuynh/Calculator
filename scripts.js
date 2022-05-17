c516da226d58921a7189315fc95f2c82d6a1fc72
miltonhuynh <miltonhuynh@gmail.com>
2022-05-16 21:24:11 -0700

    Added more numbers

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let num1 = null;
 let num2 = null;
 
+const equal = document.getElementById('equal');
+
 const digit_0 = document.getElementById('digit_0');
 const digit_1 = document.getElementById('digit_1');
 const digit_2 = document.getElementById('digit_2');
@@ -15,9 +17,80 @@ const digit_9 = document.getElementById('digit_9');
 digit_1.onclick = function() {
     if (num1 === null && num2 === null) {
         num1 = 1;
-        num1.toString();
+        num1 = num1.toString();
+    } else {
         num1 += 1;
-    } 
-    document.getElementById('test').innerHTML = num1;
-    console.log(num1);
+    }
+}
+
+digit_2.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 2;
+        num1 = num1.toString();
+    } else {
+        num1 += 2;
+    }
+}
+
+digit_3.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 3;
+        num1 = num1.toString();
+    } else {
+        num1 += 3;
+    }
+}
+
+digit_4.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 4;
+        num1 = num1.toString();
+    } else {
+        num1 += 4;
+    }
+}
+
+digit_5.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 5;
+        num1 = num1.toString();
+    } else {
+        num1 += 5;
+    }
+}
+
+digit_6.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 6;
+        num1 = num1.toString();
+    } else {
+        num1 += 6;
+    }
+}
+
+digit_7.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 7;
+        num1 = num1.toString();
+    } else {
+        num1 += 7;
+    }
+}
+
+digit_8.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 8;
+        num1 = num1.toString();
+    } else {
+        num1 += 8;
+    }
+}
+
+digit_9.onclick = function() {
+    if (num1 === null && num2 === null) {
+        num1 = 9;
+        num1 = num1.toString();
+    } else {
+        num1 += 9;
+    }
 }
